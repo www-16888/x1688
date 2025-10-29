016974f04b0b9812f57bf9c1290e4ad25a126630
www-16888 <xiaotao0310@gmail.com>
2025-10-29 19:07:14 +0800

    2

---
+++ js/baidu_tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f80418b9fe9f40e2f175df63e3dd893e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
